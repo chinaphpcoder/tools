7321af5483392da247d5c0885b8c177ee4964809
xushuai <xu.shuai@shaxiaoseng.cn>
2018-08-08 14:03:07 +0800

    test

---
+++ database/migrations/2018_08_08_134530_create_user_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',255)->default('')->comment('用户姓名');
+            $table->string('email',255)->default('')->comment('邮箱');
+            $table->string('password',255)->default('')->comment('密码');
+            $table->string('remember_token',255)->nullable()->comment('登录token');
+            $table->timestamps('created_at')->useCurrent()->comment('创建时间');
+            $table->timestamp('update_at')->comment('更新时间');
+            $table->->charset('utf8mb4');
+            $table->->collation('utf8mb4_unicode_ci');
+        });
+    }
+// CREATE TABLE `tools_users` (
+//   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+//   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+//   `test` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+//   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+//   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+//   `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+//   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+//   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+//   PRIMARY KEY (`id`),
+//   UNIQUE KEY `users_email_unique` (`email`)
+// ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
